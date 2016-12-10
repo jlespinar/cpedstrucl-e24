7ee915ab80e7898ba2bef0b68b4a5a60717d85e8
Jan Lislie Espinar <janlislieespinar14@gmail.com>
2016-12-10 10:50:42 +0800

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-	cout << "Hello World" <<endl;
-	cout << "CPEDSTRUCL";
+	cout << "Hello World" << endl;
+	cout << "CPEDSTRUCL" <<;
 	return 0;
 }

be3d27971b608067fd8de53130f8a16a2b6b3a1d
Jan Lislie Espinar <janlislieespinar14@gmail.com>
2017-02-21 21:15:07 +0800

    Update insertion_sort.cpp

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 using namespace std;
 int main(){
-	int j,i,n;
-	int A[] = {30, 40, 20, 50, 10 };
-	for (i=1; i<6; i++)
-	{
+	int j,i;
+	int A[] = { 30, 40, 20, 50, 10};
+	for (i=1; i<5; i++){
 		int tmp=A[i];
-		for (j=i-1; j>0 && tmp<A[j]; j--)
+		for (j=i-1; j>=0 && tmp<A[j]; j--)
 		{
 			A[j+1] = A[j];
 		}
 		A[j+1] = tmp;
-		}
+	}
 	cout << "Sorting:\n";
-	for (i=1; i<6; i++)
+	for (i=0; i<5; i++)
 	{
 		cout << A[i] << " " << endl;
 	}

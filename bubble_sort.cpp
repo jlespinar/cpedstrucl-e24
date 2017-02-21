a9e69aa0ef8f58f1ea24864a34e516f16982604f
Jan Lislie Espinar <janlislieespinar14@gmail.com>
2017-02-21 21:13:54 +0800

    Update bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 int main(){
-	int A[] = { 35, 10, 55, 20, 5 }, j, i;
-	{for (int i=5; i<5; i--)
-		for (int j=0; j<5-1; j++)
+	int A[] = {35, 10, 55, 20, 5};
+	for (int i=5; i>1; i--)
+		for (int j=0; j<i - 1; j++)
 			if (A[j] > A[j+1])
 			{
 				int swap = A[j];
 				A[j] = A[j+1];
 				A[j+1] = swap;
 			}
-	}
-	cout << "Sorted array element:\n ";
-	for(i=0; i<6-1; i++)
-		cout << A[i] << " " << endl;;
+	
+	cout << "Sorted array element: ";
+	for(int i=0; i<5; i++)
+		cout << A[i] << " ";
 	return 0;
 }

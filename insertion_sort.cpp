2caad1aa4c48d484a9eb7311222bce42c3cb3a2b
Jan Lislie Espinar <janlislieespinar14@gmail.com>
2017-03-04 10:39:49 +0800

    Update insertion_sort.cpp

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,22 +1,27 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int A[]= {40, 30, 20, 50, 10};
-	int q=1, x, y, w=5;
+	int A[]= {35, 10, 55, 20, 5};
+	int w=1, x, y, z=5;
+	
 	cout<<"Array: ";
 	for(int k=0; k<5; k++)
 		cout<<A[k]<<" ";
 		cout<<endl<<endl;
+	
 	for (int i=1; i<5; i++){
 		int j, temp=A[i];
 		for(j=i-1; j>=0 && temp<A[j]; j--){
 			A[j+1] = A[j];
 			x=A[j];
 			y=temp;
+			
 		}
-		A[j+1] = temp;			
-		cout<<"Pass "<<q++<<": ";
-		for (int k=0; k<w; k++){
+		A[j+1] = temp;
+		cout<<"Swapped: "<<x<<" and "<<y<<endl;
+			
+		cout<<"Pass "<<w++<<": ";
+		for (int k=0; k<z; k++){
 			cout<<A[k]<<" ";
 		}
 		cout<<endl;

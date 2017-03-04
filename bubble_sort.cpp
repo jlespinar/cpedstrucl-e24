982bb45df31e84258812e8a95810158ff551de36
Jan Lislie Espinar <janlislieespinar14@gmail.com>
2017-03-04 10:40:08 +0800

    Update bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -3,10 +3,12 @@ using namespace std;
 int main(){
 	int A[]={35, 10, 55, 20, 5};
 	int n=1, swap;
+
 	cout<<"Array: ";
 	for(int k=0; k<5; k++)
 		cout<<A[k]<<" ";
 		cout<<endl<<endl;
+
 	for (int i=5; i>1; i--){
 		for(int j=0; j<i-1; j++){
 			if(A[j]>A[j+1]){
@@ -15,10 +17,15 @@ int main(){
 				A[j+1]=swap;
 			}
 		}
+		for(int j = 0; j<i-1; j++)
+			cout<<"Swapped: "<<A[j]<<" and "<<A[j+1]<<endl;
+	
 		cout<<"Pass "<<n++<<": ";
+	
 		for(int k=0; k<5; k++)
 			cout<<A[k]<<" ";
 			cout<<endl;
+			cout << endl;
 	}
 	return 0;
 }
